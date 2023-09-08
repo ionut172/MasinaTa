f361971c1d25267cc8e47eee71d649dc8796c3a5
ionut172 <ionutgabriel172@gmail.com>
2023-09-08 11:36:28 +0300

    http.polly

--- src/LicitatiiService/Program.cs
+++ src/LicitatiiService/Program.cs
@@ -4,18 +4,24 @@ using LicitatiiService.RequestHelpers;
 using Microsoft.EntityFrameworkCore;
 using MongoDB.Driver;
 using MongoDB.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<LicitatiiDBContext>(opt=>{
+// Se adauga serviciul de licitatii cu parametri de configurare pentru string connection din appsettings.json pentru postgresql
+builder.Services.AddDbContext<LicitatiiDBContext>(opt =>
+{
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddEndpointsApiExplorer();
+// Se adauga automapper-ul Automapper;
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-builder.Services.AddMvc().AddJsonOptions(opt => {
+builder.Services.AddMvc().AddJsonOptions(opt =>
+{
+    // Face suppress la json values by default ca $id, $values;
     opt.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
 });
 
@@ -24,12 +30,15 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
-try{
-DBInitializer.InitDB(app);
+try
+{
+    DBInitializer.InitDB(app);
 }
-catch(Exception ex) {
+catch (Exception ex)
+{
     Console.WriteLine(ex.Message);
     throw new Exception("Eroare", ex);
 }
 
 app.Run();
+
--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,5 +1,8 @@
+using System.Net;
 using MongoDB.Driver;
 using MongoDB.Entities;
+using Polly;
+using Polly.Extensions.Http;
 using SearchService.Data;
 using SearchService.Models;
 using SearchService.Services;
@@ -11,17 +14,28 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddControllers(options => options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true);
-builder.Services.AddHttpClient<LicitatiiHttpClient>();
+// 2. se adauga metoda 
+builder.Services.AddHttpClient<LicitatiiHttpClient>().AddPolicyHandler(GetPolicy());
 var app = builder.Build();
 
 app.UseAuthorization();
 
 app.MapControllers();
-try{
-DBInitializer.InitDB(app);
-}
-catch(Exception ex){
-    Console.WriteLine(ex.ToString());
-}
+// 3. Se adauga lifetime-ul aplicatiei, astfel incat serviciul sa ruleze cu datele deja existente, chiar daca nu preia date actuale.
+app.Lifetime.ApplicationStarted.Register(async () =>
+{
+    try
+    {
+        await DBInitializer.InitDB(app);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.ToString());
+    };
+});
 app.Run();
+// 1. Microsoft.Extensions.Http.Polly - asigura conexiunea independenta intre doua servicii;
+// 1. Metoda HandleTransientHttpError -> handle erorr /// sau incarca permanent la fiecare 3 secunde.
+static IAsyncPolicy<HttpResponseMessage> GetPolicy() => HttpPolicyExtensions.HandleTransientHttpError().OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound).WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
